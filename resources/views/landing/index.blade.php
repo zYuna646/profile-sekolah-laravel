3bdf6d1628d4a9b1a19bcc50c5204bca35678baa
starsshinee <lintangcahyaniputri131@gmail.com>
2023-11-11 21:05:27 +0700

    secon commit

---
+++ resources/views/landing/index.blade.php
@@ -0,0 +1,305 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="UTF-8" />
+    <!-- <meta name="viewport" content="width=device-width, initial-scale=1.0">-->
+    <!-- <link rel="stylesheet" href="style.css" /> -->
+    <title>SMKN MODEL GORONTALO></title>
+
+    <!---- bootstrap -->
+    <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css" /> -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <!---- owl carousel -->
+    <!-- <link rel="stylesheet" href="assets/css/owl.carousel.min.css" /> -->
+    <!---- fontawesome/ icon ---->
+    {{-- <link rel="stylesheet" href="assets/fontawesome/css/all.min.css" /> --}}
+    <!---- style css custom -->
+    <link rel="stylesheet" href="assets/css/style.css" />
+
+    {{-- <script src="assets/js/jquery-3.3.1.slim.min.js"></script> --}}
+</head>
+
+<body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-white sticky-top">
+        <div class="container-fluid" style="padding-left: 80px; padding-right: 80px;">
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown"
+                aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse justify-content-center" id="navbarNavDropdown">
+                <ul class="navbar-nav gap-4">
+                    <li class="nav-item">
+                        <a class="nav-link nav-active fw-bold" aria-current="page" href="#">Home</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdownMenuLink"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Akademik
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Jumlah Siswa</a></li>
+                            <li><a class="dropdown-item" href="#">Prestasi</a></li>
+                            <li><a class="dropdown-item" href="#">Link E-Raport</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">Kompetensi Keahlian</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle fw-bold" href="#" id="navbarDropdownMenuLink"
+                            role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Staff
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <li><a class="dropdown-item" href="#">Struktur Organisasi</a></li>
+                            <li><a class="dropdown-item" href="#">Guru</a></li>
+                            <li><a class="dropdown-item" href="#">Tata Usaha</a></li>
+                        </ul>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link fw-bold" href="#">Galeri Kegiatan</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+
+    <header>
+        <div class="container" style="height:100vh;">
+            <div class="d-flex flex-row" style="height: 100%">
+                <div class="d-flex align-items-center gap-4">
+                    <img src="assets/img/SMAN MODEL LOGO.png" alt="" style="width: 150px">
+                    <div class="d-flex flex-column">
+                        <h4>Sistem Informasi</h4>
+                        <h1>SMK NEGERI <span style="color: #D37E00">MODEL GORONTALO</span></h1>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!--countener punya-->
+    </header>
+
+
+    <!--- bagian banner -->
+    <section>
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+            <div class="carousel-indicators">
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+              <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+            </div>
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="assets/img/smamodel.jpeg" class="d-block w-100" alt="..." style="height: 700px">
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/smamodel.jpeg" class="d-block w-100" alt="..." style="height: 700px">
+              </div>
+              <div class="carousel-item">
+                <img src="assets/img/smamodel.jpeg" class="d-block w-100" alt="..." style="height: 700px">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+    </section>
+
+    <script src="assets/js/bootstrap.min.js"></script>
+    <!-- <script src="assets/js/owl.carousel.min.js"></script> -->
+
+    <!---kenapa harus sekolah-->
+
+    <div class="container-fluid" style="background-color: #007C23;">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-1">
+
+                </div>
+                <div class="col-lg-10 text-center mt-4 text-light">
+                    <h3>Kenapa Harus Sekolah di <span class="text-warning"> SMKN Model Gorontalo </span> ?</h4>
+                        <p>Alasan kenapa kalian harus bergabung di SMKN Model Gorontalo</p>
+
+                </div>
+                <div class="col-lg-1">
+
+                </div>
+            </div>
+            <!-- panggil class row untuk membuat ruang 12 kolom -->
+            <div class="row">
+                <!-- ini menggunakan 3 kolom yang tersedia -->
+                <div class="col-lg-3 ">
+                    <div class="rounded p-4 shadow">
+                        <div>
+                            <div class="d-flex justify-content-center ">
+                                <div class="circle-icon">
+                                    
+                                </div>
+                            </div>
+                            <div class="text-light">
+                                <h4 class="mt-4">Akreditas</h4>
+                                <p class="mt-3">Sekolah Kami saat ini sudah terverifikasi dan terakreditasi A.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 ">
+                    <div class="rounded p-4 shadow">
+                        <div>
+                            <div class="logo d-flex justify-content-center ">
+                                <div class="circle-icon">
+                                    
+                                </div>
+                            </div>
+                            <div class="text-light">
+                                <h4 class="mt-4">Pengajar Kompeten</h4>
+                                <p class="mt-3">Guru yang Update dengan perkembangan industri.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 ">
+                    <div class="rounded p-4 shadow">
+                        <div>
+                            <div class="logo d-flex justify-content-center ">
+                                <div class="circle-icon">
+                                    
+                                </div>
+                            </div>
+                            <div class="text-light">
+                                <h4 class="mt-4">Kerjasama Luas</h4>
+                                <p class="mt-3">Memperbesar kesempatan para peserta didik untuk bisa bekerja sebelum
+                                    lulus.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-3 ">
+                    <div class="rounded p-4 shadow">
+                        <div>
+                            <div class="logo d-flex justify-content-center ">
+                                <div class="circle-icon">
+                                    
+                                </div>
+                            </div>
+                            <div class="text-light">
+                                <h4 class="mt-4">Fasilitas Lengkap</h4>
+                                <p class="mt-3">Fasilitas pembelajaran yang lengkap dan terjamin.</p>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--untuk Visi Misi-->
+    <div class="container-fluid">
+        <div class="container">
+            <div class="row ">
+                <div class="col-lg-1">
+
+                </div>
+                <div class="col-lg-10 text-center mt-4 text-dark">
+                    <h2> VISI MISI <span class="text-success">SMKN Model Gorontalo </span></h4>
+                </div>
+                <div class="col-lg-7">
+
+                </div>
+            </div>
+
+
+            <!--row punya isi visi misi-->
+            <div class="row">
+                <!-- ini menggunakan 3 kolom yang tersedia -->
+                <div class="col-lg-6 ">
+                    <div>
+                        <div class=" justify-content-center ">
+                            <h3 class="bg-secondary w-25">VISI</h2>
+                                <P>Terwujudnya SMK Negeri Model Gorontalo yang inovatif, unggul dan berbudaya</P>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-lg-6 ">
+                    <div>
+                        <div class=" justify-content-center ">
+                            <h3 class="bg-secondary w-50">MISI</h2>
+                                <P>Membangun sistem manejemen pengelolaan SMK yang handal
+                                    <br>Menyiapkan guru profesional yang memiliki kompetensi kejuruan <br>
+                                    Menyelenggarakan pembelajaran sesuai kompetensi dan tuntutan pasar kerja
+                                    <br> Menjalin hubungan kerja sama yang erat dengan dunia usaha dan industri </br>
+                                </P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Punya Sambutan kepsek-->
+    <div class="container-fluid p-0 mt-3">
+        <div class="row">
+            <div class="col-lg-8 p-4" style="background-color: #D37E00; border-radius: 0px 50px 50px 0px;">
+                <div>
+                    <div class=" ">
+                        <h3 class="text-light">Sambutan Kepala Sekolah</h2>
+                            <P class="text-light">Assalamu'alaikum Wr. Wb.</P>
+                            <p>Puji Syukur Alhamdulillah kita panjatkan kehadirat Allah SWT, atas segala rahmat dan
+                                hidayah-Nya sehingga kita masih diberi kesempatan untuk bekerja dan berkarya di bidang
+                                pendidikan. Karena pendidikan merupakan upaya kita untuk menyiapkan generasi penerus
+                                bangsa, yang akan menggantikan estafet kepemimpinan kita di masa mendatang.
+                                Kami mengucapkan selamat datang di website resmi SMK Jakarta Pusat 1 yang dapat
+                                digunakan sebagai salah satu media komunikasi, dan interaksi antara civitas akademika
+                                dan masyarakat pada umumnya.
+                                Mari kita bekerja dan berkarya dengan mengharap ridho-Nya dan keikhlasan yang tulus demi
+                                menyiapkan generasi bangsa yang berintegritas dan berprestasi.
+                                Wassalamu'alaikum Wr. Wb</p>
+                            <p class="text-light">Suleman Mayang M.Pd</p>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3 p-4">
+                <div>
+                    <div class="">
+                        <img src="assets/img/kepsek.jpg" alt="#" style="height:300px"
+                            class="rounded float/right wt-50">
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+    </div>
+    </section>
+    <!--bagian bawah-->
+    <footer>
+        <div class="container-fluid p-0 mt-3">
+            <div class="row">
+                <div class="col-lg-12 p-4" style="background-color:black">
+                    <div>
+                        <div class=" ">
+                            <img src="assets/img/SMAN MODEL LOGO.png" alt="#">
+                            <P class="text-light">SMA NEGERI MODEL GORONTALO
+                                <br>Jl. Muchlis Rahim, Desa Panggulo Barat – Botupingge Kab Bone Bolango – Provinsi
+                                Gorontalo 96554 Indonesia
+                            </p>
+                        </div>
+                        <div class=" ">
+                            <P class="text-light">follow Us</P>
+                            <img src="assets/img/facebook-instagram-twitter-icons-png-12 1.png" alt="#">
+                        </div>
+                    </div>
+    </footer>
+</body>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"
+    integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous">
+</script>
+
+</html>
