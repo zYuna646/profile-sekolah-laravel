2ff71d6b923d9c226c406bf2dbfbaf810529f278
Dhnzz <dhanisyahdila@gmail.com>
2023-11-12 23:19:04 +0800

    landing-fix

--- resources/views/landing/index.blade.php
+++ resources/views/landing/index.blade.php
@@ -11,6 +11,7 @@
     <!-- <link rel="stylesheet" href="assets/css/bootstrap.min.css" /> -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css" integrity="sha512-z3gLpd7yknf1YoNbCzqRKc4qyor8gaKU1qmn+CShxbuBusANI9QpRohGBreCFkKxLhei6S9CQXFEbbKuqLg0DA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
     <!---- owl carousel -->
     <!-- <link rel="stylesheet" href="assets/css/owl.carousel.min.css" /> -->
     <!---- fontawesome/ icon ---->
@@ -39,7 +40,6 @@
                             Akademik
                         </a>
                         <ul class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-                            <li><a class="dropdown-item" href="#">Jumlah Siswa</a></li>
                             <li><a class="dropdown-item" href="#">Prestasi</a></li>
                             <li><a class="dropdown-item" href="#">Link E-Raport</a></li>
                         </ul>
@@ -101,6 +101,62 @@
         </div>
         <!--countener punya-->
     </section>
+
+    <div class="container-fluid pb-5 px-4 bg-light">
+        <div class="row">
+            <div class="col">
+                <div class="rounded shadow p-4">
+                    <div class="row">
+                        <div class="col-auto d-flex align-item-center">
+                            <i class="fa-solid fa-graduation-cap fa-2xl my-auto text-success"></i>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <h1>Jurusan</h1>
+                            </div>
+                            <div class="row">
+                                <h2>5</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="rounded shadow p-4">
+                    <div class="row">
+                        <div class="col-auto d-flex align-item-center">
+                            <i class="fa-solid fa-users fa-2xl my-auto text-primary"></i>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <h1>Siswa</h1>
+                            </div>
+                            <div class="row">
+                                <h2>5</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="rounded shadow p-4">
+                    <div class="row">
+                        <div class="col-auto d-flex align-item-center">
+                            <i class="fa-solid fa-user-graduate fa-2xl my-auto text-warning"></i>
+                        </div>
+                        <div class="col">
+                            <div class="row">
+                                <h1>Tim Pengajar</h1>
+                            </div>
+                            <div class="row">
+                                <h2>5</h2>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 
 
     <!--- bagian banner -->
