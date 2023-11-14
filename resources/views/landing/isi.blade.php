f35e767dc7b247a75dfbb6886e22794de2496fd0
starsshinee <lintangcahyaniputri131@gmail.com>
2023-11-14 20:06:48 +0700

    up

--- resources/views/landing/isi.blade.php
+++ resources/views/landing/isi.blade.php
@@ -10,4 +10,153 @@
         </div>
     </div>
     <hr class="mx-5 bg-danger border-5 border-top border-dark">
+
+    <h1 class="display-8 fw-bold lh-1 mb-3 text-center" style="color: #D37E00"> Agribisnis Tanaman Pangan Dan Hortikultura
+        (ATPH)</h1>
+    <div class="container px-5">
+        <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+            <div class="col d-flex">
+                <div class="  card border-kompetensi">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                        <h3 class=" w-20 ps-3 rounded">Sasaran Hasil Pembelajaran</h3>
+                        <P>Menjadikan Kompetensi Keahlian terampil, handal, mandiri betanggung jawab, terpecaya dan
+                            berakhlak mulia
+                        </P>
+                    </div>
+                </div>
+            </div>
+        
+
+        
+            <div class="col d-flex">
+                <div class="  card border-kompetensi">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                        <h3 class="w-20 ps-3 rounded">Gambar Kegiatan Pembelajaran</h3>
+                        <img src="assets/img/kegiatan 1.jpeg" alt="" class="card-img-top mx-auto"
+                            style="width: 200px">
+                        </P>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+    </div>
+
+    <h1 class="display-8 fw-bold lh-1 mb-3 text-center" style="color: #D37E00">Agribisnis Ternak Ruminansia (ATR)</h1>
+    <div class="container px-5">
+        <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+            <div class="col d-flex">
+                <div class="  card border-kompetensi">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                        <h3 class=" w-20 ps-3 rounded">Sasaran Hasil Pembelajaran</h3>
+                        <P>Terwujudnya lulusan yang mandiri dan kompeten di bidang Agribisnis Peternakan serta memanfaatkan,
+                            mengembangkan, dan melestarikan sumber daya alam dengan sentuhan teknologi untuk mengfhasilkan
+                            generasi
+                            yang bertakwa jujur dan bertanggung jawab </P>
+                    </div>
+                </div>
+            </div>
+            <div class="col d-flex">
+                <div class="  card border-kompetensi">
+                    <div class="card-body text-center">
+                        <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                        <h3 class="w-20 ps-3 rounded">Gambar Kegiatan Pembelajaran</h3>
+                        <img src="assets/img/kegiatan 6.jpeg" alt="" class="card-img-top mx-auto"
+                            style="width: 200px">
+                        </P>
+                    </div>
+                </div>
+            </div>
+        </div>
+
+
+        <h1 class="display-8 fw-bold lh-1 mb-3 text-center" style="color: #D37E00"> Agribisnis Pengolahan Hasil Pertanian
+            (APHP)
+        </h1>
+        <div class="container px-5">
+            <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+                <div class="col d-flex">
+                    <div class="  card border-kompetensi">
+                        <div class="card-body text-center">
+                            <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                            <h3 class=" w-20 ps-3 rounded">Sasaran Hasil Pembelajaran</h3>
+                            <P>Menghasilkan tamatan yang mandiri dan berjiwa wirausaha dalam pengolahan hasil pertanian</P>
+                        </div>
+                    </div>
+                </div>
+                <div class="col d-flex">
+                    <div class="  card border-kompetensi">
+                        <div class="card-body text-center">
+                            <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                            <h3 class="w-20 ps-3 rounded">Gambar Kegiatan Pembelajaran</h3>
+                            <img src="assets/img/kegiatan 4.jpeg" alt="" class="card-img-top mx-auto"
+                                style="width: 200px">
+                            </P>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <h1 class="display-8 fw-bold lh-1 mb-3 text-center" style="color: #D37E00">Agribisnis Ternak Unggas (ATU) </h1>
+            <div class="container px-5">
+                <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+                    <div class="col d-flex">
+                        <div class="  card border-kompetensi">
+                            <div class="card-body text-center">
+                                <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                                <h3 class=" w-20 ps-3 rounded">Sasaran Hasil Pembelajaran</h3>
+                                <P>Menghasilkan tamatan yang mandiri dan berjiwa wirausaha, mampu bersaing di era digital
+                                    dan global dalam
+                                    pengelolaan Agribisnis Ternak Unggas.</P>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col d-flex">
+                        <div class="  card border-kompetensi">
+                            <div class="card-body text-center">
+                                <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                                <h3 class="w-20 ps-3 rounded">Gambar Kegiatan Pembelajaran</h3>
+                                <img src="assets/img/kegiatan 6.jpeg" alt="" class="card-img-top mx-auto"
+                                    style="width: 200px">
+                                </P>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+
+                <h1 class="display-8 fw-bold lh-1 mb-3 text-center" style="color: #D37E00"> Teknologi Komputer dan Jaringan
+                    (TKJ)</h1>
+                <div class="container px-5">
+                    <div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+                        <div class="col d-flex">
+                            <div class="  card border-kompetensi">
+                                <div class="card-body text-center">
+                                    <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                                    <h3 class=" w-20 ps-3 rounded">Sasaran Hasil Pembelajaran</h3>
+                                    <P>Mencetak tenaga kerja di bidang Teknik Komputer dan Jaringan yang berkarakter,
+                                        berahlaq mulia, dan
+                                        terampil</P>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="col d-flex">
+                            <div class="card border-kompetensi">
+                                <div class="card-body text-center">
+                                    <i class="fa-solid fa-computer fa-2xl mb-5 mt-4" style="color: #D37E00"></i>
+                                    <h3 class="w-20 ps-3 rounded">Gambar Kegiatan Pembelajaran</h3>
+                                    <img src="assets/img/kegiatan 8.jpeg" alt="" class="card-img-top mx-auto"
+                                        style="width: 200px">
+                                    </P>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
 @endsection
