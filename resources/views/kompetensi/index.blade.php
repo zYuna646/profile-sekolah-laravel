62a1ee5e4f1808e60c6ddab2c56d46a7ccf9525c
Dhnzz <dhanisyahdila@gmail.com>
2023-11-16 01:30:40 +0800

    upup

--- resources/views/kompetensi/index.blade.php
+++ resources/views/kompetensi/index.blade.php
@@ -7,12 +7,14 @@
         <div class="table-responsive">
             <h2 class="fw-bolder text-primary">Data Kompetensi Keahlian</h2>
             <a href="{{route('dashboard.kompetensi.create')}}" class="btn btn-success my-4"><i class="ti ti-plus"></i> Tambah Data</a>
-            <table class="table" id="tabel">
-                <thead>
+            <table class="table table-bordered" id="tabel">
+                <thead class="bg-primary text-light">
                     <tr>
-                        <th scope="col">Column 1</th>
-                        <th scope="col">Column 2</th>
-                        <th scope="col">Column 3</th>
+                        <th scope="col">No.</th>
+                        <th scope="col">Nama Kompetensi</th>
+                        <th scope="col">Deskripsi</th>
+                        <th scope="col">Foto</th>
+                        <th scope="col">Opsi</th>
                     </tr>
                 </thead>
                 <tbody>
@@ -20,11 +22,12 @@
                         <td scope="row">R1C1</td>
                         <td>R1C2</td>
                         <td>R1C3</td>
-                    </tr>
-                    <tr class="">
-                        <td scope="row">Item</td>
-                        <td>Item</td>
-                        <td>Item</td>
+                        <td>R1C4</td>
+                        <td>
+                            <button class="btn btn-info btn-sm" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="Detail"><i class="ti ti-eye fw-bolder"></i></button>
+                            <button class="btn btn-warning btn-sm"><i class="ti ti-edit fw-bolder"></i></button>
+                            <button class="btn btn-danger btn-sm"><i class="ti ti-trash fw-bolder"></i></button>
+                        </td>
                     </tr>
                 </tbody>
             </table>
